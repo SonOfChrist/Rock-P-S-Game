2083c6955d4c08a990a710f7e4a3007d27fe9262
SoftwareDev Stan <mochogestanley80@gmail.com>
2024-09-20 10:08:22 +0300

    Update-js-Auto-Play

--- README.md
+++ README.md
@@ -7,4 +7,7 @@
 - You pick and a Move a The Computer will Randomly pick its Move
 - The game is Just easy, It just See's Who Won, Tied and Lost the Game 
 <img src="Images/Rock_Paper_Scissors__Finally.png" alt="FinalProject" class="Final_Project">
--
+
+# setInterval || Timeout
+- This will just make the game to play by itself {AutoPlay}
+<img src="./Images/AutoPlay.png" alt="js-AutoPlay-Player-Computer_Move">
--- main.js
+++ main.js
@@ -2,6 +2,21 @@ let score = JSON.parse(localStorage.getItem('score')) || { wins:0, losses:0, tie
 
 updateScoreElement();
 
+let isAutoPlaying = false
+let intervalId;
+function autoPlay () {
+    if (!isAutoPlaying){
+        intervalId =setInterval(function(){
+            const playerMove = pickComputerMoves();
+            playGame(playerMove);
+        }, 1000);
+        isAutoPlaying = true
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false
+    }
+}
+
 function playGame(playerMove){
     const computerMove = pickComputerMoves();
 
